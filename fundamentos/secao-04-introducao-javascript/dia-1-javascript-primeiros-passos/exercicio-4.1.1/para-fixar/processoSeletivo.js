3df39d326744d6d6138c2b862b9d9cf1b0fd91a0
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-09-21 19:31:37 -0300

    para  Fixar -> dia 1 introdução ao javascript

---
+++ fundamentos/secao-04-introducao-javascript/dia-1-javascript-primeiros-passos/exercicio-4.1.1/para-fixar/processoSeletivo.js
@@ -0,0 +1,15 @@
+let statusAluno = 'aprovada';
+
+switch (statusAluno) {
+  case 'aprovada':
+    console.log('Parabéns, você foi aprovada(o)!');
+    break
+  case 'lista':
+    console.log('Você está na nossa lista de espera');
+    break
+  case 'reprovada':
+    console.log('Você foi reprovada(o)');
+    break
+  default:
+    console.log('Informação incorreta');
+}
